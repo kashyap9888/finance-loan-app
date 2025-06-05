01cb37c004fa0e08bcc8764e7fad43278ca55c58
kashyap9888 <69461868+kashyap9888@users.noreply.github.com>
2025-06-05 20:14:50 +1000

    Add .next/static/chunks/src_app_admin_page_tsx_9148afbf._.js 1749118489352

---
+++ .next/static/chunks/src_app_admin_page_tsx_9148afbf._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_app_admin_MapComponent_tsx_b7d290da._.js",
+  "static/chunks/_6fd9cf64._.js"
+],
+    source: "dynamic"
+});
